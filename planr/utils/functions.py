19f2e29d373de1d518b3aacc5f054ff9f6bd4f2f
cheetahbyte <bernerdoodle@outlook.de>
2021-12-22 10:05:54 +0100

    fix(security): removed unnessecary line of code

--- planr/utils/functions.py
+++ planr/utils/functions.py
@@ -10,5 +10,3 @@ def generate_password(length=8, chars=string.ascii_letters + string.digits):
 def generate_hash(password) -> tuple:
     salt: bytes = os.urandom(32)
     return salt, hashlib.scrypt(password.encode(), salt=salt, n=2**14, r=8, p=1).hex()
-
-print(generate_hash("leoisderbesteundlauradieschönste"))
